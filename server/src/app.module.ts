5b06746614ead31a35c9779f11388aaa85b50e20
adhimrahman <adhimrahaman@gmail.com>
2025-07-11 02:17:54 +0800

    feat(app): import UsersModule and AuthModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,6 +20,9 @@ import { AppService } from './app.service';
       autoLoadEntities: true,
       synchronize: true,
     }),
+
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
