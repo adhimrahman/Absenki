fb4c340e424a66750f9042d2bd7d235f969adca6
adhimrahman <adhimrahaman@gmail.com>
2025-07-12 23:07:50 +0800

    feat(users): implement User entity, UsersService, and UsersController with admin seeding functionality

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+  ) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userRepo.findOne({ where: { email } });
+  }
+
+  // Tambahan opsional
+  async findAll(): Promise<User[]> {
+    return this.userRepo.find();
+  }
+
+  async findById(id: number): Promise<User | null> {
+    return this.userRepo.findOne({ where: { id } });
+  }
+
+  create(data: Partial<User>) {
+    const user = this.userRepo.create(data);
+    return this.userRepo.save(user);
+  }
+}
